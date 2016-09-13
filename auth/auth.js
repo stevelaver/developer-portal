9ee031087d614b4bae403bab142ba774dcc383a5
Jakub Matejka <jakub@keboola.com>
2016-09-13 16:18:38 +0200

    auth in sls 1.0

--- auth/auth.js
+++ auth/auth.js
@@ -1,4 +1,6 @@
 'use strict';
+require('dotenv').config();
+
 var async = require('async');
 var aws = require('aws-sdk');
 var identity = require('lib/identity');
@@ -12,8 +14,11 @@ const vandium = require('vandium');
  */
 module.exports.confirm = vandium.createInstance({
   validation: {
-    email: vandium.types.string().required().error(new Error("Parameter email is required")),
-    code: vandium.types.string().required().error(new Error("Parameter code is required"))
+    path: vandium.types.object().keys({
+      email: vandium.types.string().required().error(new Error("[422] Parameter email is required and should have " +
+        "format of email address")),
+      code: vandium.types.string().required().error(new Error("[422] Parameter code is required"))
+    })
   }
 }).handler(function(event, context, callback) {
   var provider = new aws.CognitoIdentityServiceProvider({region: process.env.REGION});
@@ -21,8 +26,8 @@ module.exports.confirm = vandium.createInstance({
     function (callbackLocal) {
       provider.confirmSignUp({
         ClientId: process.env.COGNITO_CLIENT_ID,
-        ConfirmationCode: event.code,
-        Username: event.email
+        ConfirmationCode: event.path.code,
+        Username: event.path.email
       }, function(err) {
         return callbackLocal(err);
       });
@@ -30,7 +35,7 @@ module.exports.confirm = vandium.createInstance({
     function(callbackLocal) {
       provider.adminDisableUser({
         UserPoolId: process.env.COGNITO_POOL_ID,
-        Username: event.email
+        Username: event.path.email
       }, function(err) {
         return callbackLocal(err);
       });
@@ -44,11 +49,11 @@ module.exports.confirm = vandium.createInstance({
       Destination: { ToAddresses: [process.env.SES_EMAIL] },
       Message: {
         Subject: {
-          Data: '[dev-portal] User ' + event.email + ' requests approval'
+          Data: '[dev-portal] User ' + event.path.email + ' requests approval'
         },
         Body: {
           Text: {
-            Data: 'User ' + event.email + ' just signed up'
+            Data: 'User ' + event.path.email + ' just signed up'
           }
         }
       }
@@ -60,46 +65,166 @@ module.exports.confirm = vandium.createInstance({
 
 
 /**
- * Login
+ * Confirm Resend
  */
-module.exports.login = vandium.createInstance({
+module.exports.confirmResend = vandium.createInstance({
   validation: {
-    email: vandium.types.email().required().error(new Error("Parameter email is required")),
-    password: vandium.types.string().required().error(new Error("Parameter password is required"))
+    body: vandium.types.object().keys({
+      email: vandium.types.string().required().error(new Error("Parameter email is required and should have format " +
+        "of email address")),
+      code: vandium.types.string().required().error(new Error("Parameter code is required"))
+    })
   }
 }).handler(function(event, context, callback) {
-  var params = {
+  var provider = new aws.CognitoIdentityServiceProvider({region: process.env.REGION});
+  provider.adminInitiateAuth({
     AuthFlow: 'ADMIN_NO_SRP_AUTH',
     ClientId: process.env.COGNITO_CLIENT_ID,
     UserPoolId: process.env.COGNITO_POOL_ID,
     AuthParameters: {
-      USERNAME: event.email,
-      PASSWORD: event.password
+      USERNAME: event.body.email,
+      PASSWORD: event.body.password
     }
-  };
-
-  var identity = new aws.CognitoIdentityServiceProvider({region: process.env.REGION});
-  identity.adminInitiateAuth(params, function(err, data) {
-    if (err) {
-      return callback(err);
+  }, function(err) {
+    if (!err || err.code === 'NotAuthorizedException') {
+      return callback(Error('Already confirmed'));
     } else {
-      return callback(null, {
-        token: data.AuthenticationResult.AccessToken, //data.AuthenticationResult.IdToken,
-        expires: moment().add(data.AuthenticationResult.ExpiresIn, 's').utc().format()
-      });
+      if (err.code === 'UserNotConfirmedException') {
+        provider.resendConfirmationCode({
+          ClientId: process.env.COGNITO_CLIENT_ID,
+          Username: event.body.email
+        }, function(err) {
+          return callback(err);
+        });
+      } else {
+        return callback(err);
+      }
     }
   });
 });
+
+
+/**
+ * Forgot
+ */
+module.exports.forgot = vandium.createInstance({
+  validation: {
+    path: vandium.types.object().keys({
+      email: vandium.types.string().required().error(new Error("Parameter email is required and should have format " +
+        "of email address"))
+    })
+  }
+}).handler(function(event, context, callback) {
+  var provider = new aws.CognitoIdentityServiceProvider({region: process.env.REGION});
+  provider.forgotPassword({
+    ClientId: process.env.COGNITO_CLIENT_ID,
+    Username: event.path.email
+  }, function(err) {
+    return callback(err);
+  });
+});
+
+
+/**
+ * Forgot Confirm
+ */
+module.exports.forgotConfirm = vandium.createInstance({
+  validation: {
+    path: vandium.types.object().keys({
+      email: vandium.types.string().required().error(new Error('Parameter email is required and should have format ' +
+        'of email address')),
+    }),
+    body: vandium.types.object().keys({
+      password: vandium.types.string().required().error(new Error('Parameter password is required')),
+      code: vandium.types.string().required().error(new Error('Parameter code is required'))
+    })
+  }
+}).handler(function(event, context, callback) {
+  var provider = new aws.CognitoIdentityServiceProvider({region: process.env.REGION});
+  provider.confirmForgotPassword({
+    ClientId: process.env.COGNITO_CLIENT_ID,
+    ConfirmationCode: event.body.code,
+    Password: event.body.password,
+    Username: event.path.email
+  }, function(err) {
+    return callback(err);
+  });
+});
+
+
+/**
+ * Login
+ */
+module.exports.login = vandium.createInstance({
+  validation: {
+    body: vandium.types.object().keys({
+      email: vandium.types.email().required().error(new Error("Parameter email is required and should have format " +
+        "of email address")),
+      password: vandium.types.string().required().error(new Error("Parameter password is required"))
+    })
+  }
+}).handler(function(event, context, callback) {
+  var provider = new aws.CognitoIdentityServiceProvider({region: process.env.REGION});
+  provider.adminInitiateAuth({
+    AuthFlow: 'ADMIN_NO_SRP_AUTH',
+    ClientId: process.env.COGNITO_CLIENT_ID,
+    UserPoolId: process.env.COGNITO_POOL_ID,
+    AuthParameters: {
+      USERNAME: event.body.email,
+      PASSWORD: event.body.password
+    }
+  }, function(err, data) {
+    if (err) {
+      return callback(err);
+    }
+
+    return callback(null, {
+      token: data.AuthenticationResult.AccessToken, //data.AuthenticationResult.IdToken,
+      expires: moment().add(data.AuthenticationResult.ExpiresIn, 's').utc().format()
+    });
+  });
+});
+
 
 /**
  * Profile
  */
 module.exports.profile = vandium.createInstance({
   validation: {
-    token: vandium.types.string().required()
+    headers: vandium.types.object().keys({
+      authorizationToken: vandium.types.string().required()
+    })
   }
 }).handler(function(event, context, callback) {
-  identity.getUser(event.token, callback);
+  identity.getUser(event.headers.authorizationToken, callback);
+});
+
+
+/**
+ * Profile Change
+ */
+module.exports.profileChange = vandium.createInstance({
+  validation: {
+    headers: vandium.types.object().keys({
+      authorizationToken: vandium.types.string().required()
+    }),
+    body: vandium.types.object().keys({
+      oldPassword: vandium.types.string().required().error(new Error('Parameter oldPassword is required')),
+      newPassword: vandium.types.string().required().min(8)
+        .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}/)
+        .error(new Error('Parameter newPassword is required, must have at least 8 characters and contain at least one '
+          + 'lowercase letter, one uppercase letter, one number and one special character'))
+    })
+  }
+}).handler(function(event, context, callback) {
+  var provider = new aws.CognitoIdentityServiceProvider({region: process.env.REGION});
+  provider.changePassword({
+    PreviousPassword: event.body.oldPassword,
+    ProposedPassword: event.body.newPassword,
+    AccessToken: event.headers.authorizationToken
+  }, function(err) {
+    return callback(err);
+  });
 });
 
 
@@ -108,13 +233,16 @@ module.exports.profile = vandium.createInstance({
  */
 module.exports.signup = vandium.createInstance({
   validation: {
-    name: vandium.types.string().required().error(new Error('Parameter name is required')),
-    email: vandium.types.email().required().error(new Error('Parameter email is required')),
-    password: vandium.types.string().required().min(8)
-      .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}/)
-      .error(new Error('Parameter password is required, must have at least 8 characters and contain at least one '
-        + 'lowercase letter, one uppercase letter, one number and one special character')),
-    vendor: vandium.types.string().required().error(new Error('Parameter vendor is required'))
+    body: vandium.types.object().keys({
+      name: vandium.types.string().required().error(new Error('Parameter name is required')),
+      email: vandium.types.email().required().error(new Error('Parameter email is required and should have format ' +
+        'of email address')),
+      password: vandium.types.string().required().min(8)
+        .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}/)
+        .error(new Error('Parameter password is required, must have at least 8 characters and contain at least one '
+          + 'lowercase letter, one uppercase letter, one number and one special character')),
+      vendor: vandium.types.string().required().error(new Error('Parameter vendor is required'))
+    })
   }
 }).handler(function(event, context, callback) {
   var db = mysql.createConnection({
@@ -127,7 +255,7 @@ module.exports.signup = vandium.createInstance({
 
   async.waterfall([
     function(callbackLocal) {
-      db.query('SELECT * FROM `vendors` WHERE `id` = ?', [event.vendor], function(err, result) {
+      db.query('SELECT * FROM `vendors` WHERE `id` = ?', [event.body.vendor], function(err, result) {
         if (err) return callbackLocal(err);
 
         if (result.length === 0) {
@@ -141,20 +269,20 @@ module.exports.signup = vandium.createInstance({
       var provider = new aws.CognitoIdentityServiceProvider({region: process.env.REGION});
       provider.signUp({
         ClientId: process.env.COGNITO_CLIENT_ID,
-        Username: event.email,
-        Password: event.password,
+        Username: event.body.email,
+        Password: event.body.password,
         UserAttributes: [
           {
             Name: 'email',
-            Value: event.email
+            Value: event.body.email
           },
           {
             Name: 'name',
-            Value: event.name
+            Value: event.body.name
           },
           {
             Name: 'profile',
-            Value: event.vendor
+            Value: event.body.vendor
           }
         ]
       }, function(err) {
