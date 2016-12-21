abd15995cac2bc5063423f0b80d4f4df568878dd
Jakub Matejka <jakub@keboola.com>
2016-12-21 10:33:11 +0100

    :zap: Add basic functionality for ECR provisioning

--- lambda/vendor/apps/repository.js
+++ lambda/vendor/apps/repository.js
@@ -1,18 +1,20 @@
 'use strict';
 
-import App from '../../../lib/app';
+import Repository from '../../../lib/repository';
 
 require('babel-polyfill');
+const aws = require('aws-sdk');
 const db = require('../../../lib/db');
 const error = require('../../../lib/error');
 const identity = require('../../../lib/identity');
 const joi = require('joi');
-const notification = require('../../../lib/notification');
 const request = require('../../../lib/request');
 const validation = require('../../../lib/validation');
 
-const app = new App(db, process.env, error);
-notification.setHook(process.env.SLACK_HOOK_URL, process.env.SERVICE_NAME);
+aws.config.setPromisesDependency(Promise);
+const ecr = new aws.ECR({ region: process.env.REGION });
+const iam = new aws.IAM({ region: process.env.REGION });
+const repository = new Repository(db, ecr, iam, aws, process.env, error);
 
 /**
  * Create repository
@@ -25,7 +27,19 @@ module.exports.create = (event, context, callback) => request.errorHandler(() =>
     },
   });
 
-  // TODO
+  return request.responseDbPromise(
+    db.connect(process.env)
+      .then(() => identity.getUser(process.env.REGION, event.headers.Authorization))
+      .then(user => repository.create(
+        user.vendor,
+        event.pathParameters.appId,
+        user.email,
+      )),
+    db,
+    event,
+    context,
+    callback
+  );
 }, event, context, (err, res) => db.endCallback(err, res, callback));
 
 /**
@@ -39,5 +53,17 @@ module.exports.get = (event, context, callback) => request.errorHandler(() => {
     },
   });
 
-  // TODO
+  return request.responseDbPromise(
+    db.connect(process.env)
+      .then(() => identity.getUser(process.env.REGION, event.headers.Authorization))
+      .then(user => repository.get(
+        new aws.STS({ region: process.env.REGION }),
+        user.vendor,
+        event.pathParameters.appId,
+      )),
+    db,
+    event,
+    context,
+    callback
+  );
 }, event, context, (err, res) => db.endCallback(err, res, callback));
