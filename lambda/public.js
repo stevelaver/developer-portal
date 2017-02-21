77541cb377e6146d5a27eebdc0b584e7b96c0040
Jakub Matejka <jakub@keboola.com>
2017-02-21 12:28:29 +0100

    :zap: Reduce all public resources to one λ function

--- lambda/public.js
+++ lambda/public.js
@@ -1,21 +1,96 @@
 'use strict';
 
+import App from '../lib/app';
+import Identity from '../lib/identity';
+import Validation from '../lib/validation';
+
 require('babel-polyfill');
-const error = require('../../lib/error');
-const html = require('../../views/landing.html');
-const request = require('../../lib/request');
+const _ = require('lodash');
+const joi = require('joi');
+
+const db = require('../lib/db');
+const error = require('../lib/error');
+const landingHtml = require('../views/landing.html');
+const request = require('../lib/request');
+
+const app = new App(db, Identity, process.env, error);
+const validation = new Validation(joi, error);
 
 
-/**
- * Landing Page
- */
-module.exports.public = (event, context, callback) => request.htmlErrorHandler(() => {
-  if (event.resource === '/' && event.httpMethod === 'GET') {
-    return callback(null, {
-      headers: { 'Content-Type': 'text/html' },
-      body: html({ apiEndpoint: process.env.API_ENDPOINT }),
-      statusCode: 200,
-    });
+function landing(event, context, callback) {
+  return callback(null, {
+    headers: { 'Content-Type': 'text/html' },
+    body: landingHtml({ apiEndpoint: process.env.API_ENDPOINT }),
+    statusCode: 200,
+  });
+}
+
+function detail(event, context, callback) {
+  validation.validate(event, {
+    path: {
+      vendor: joi.string().required(),
+      app: joi.string().required(),
+    },
+  });
+
+  return request.responseDbPromise(
+    db.connect(process.env)
+      .then(() => app.getAppWithVendor(
+        event.pathParameters.app,
+        null,
+        true
+      )),
+    db,
+    event,
+    context,
+    callback
+  );
+}
+
+function list(event, context, callback) {
+  validation.validate(event, {
+    pagination: true,
+    query: {
+      project: joi.number().integer(),
+    },
+  });
+
+  return request.responseDbPromise(
+    db.connect(process.env)
+      .then(() => app.listPublishedApps(
+        _.get(event, 'queryStringParameters.offset', null),
+        _.get(event, 'queryStringParameters.limit', null),
+      )),
+    db,
+    event,
+    context,
+    callback
+  );
+}
+
+function stacks(event, context, callback) {
+  return request.responseDbPromise(
+    db.connect(process.env)
+      .then(() => db.listStacks()),
+    db,
+    event,
+    context,
+    callback
+  );
+}
+
+
+module.exports.public = (event, context, callback) => request.errorHandler(() => {
+  switch (event.resource) {
+    case '/':
+      return landing(event, context, callback);
+    case '/apps':
+      return list(event, context, callback);
+    case '/apps/{vendor}/{app}':
+      return detail(event, context, callback);
+    case '/stacks':
+      return stacks(event, context, callback);
+    default:
+      throw error.notFound();
   }
-  throw error.notFound();
-}, event, context, callback);
+}, event, context, (err, res) => db.endCallback(err, res, callback));
