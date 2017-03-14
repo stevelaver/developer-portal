9a4814c13fc77c74c4aea47a4a75dc191806e5ec
Jakub Matejka <jakub@keboola.com>
2017-03-14 09:14:56 +0100

    :bug: Improve db migrations

--- scripts/migrate-sql.js
+++ scripts/migrate-sql.js
@@ -6,11 +6,15 @@ const path = require('path');
 const MigrateSql = module.exports;
 
 MigrateSql.migrate = (db, file) => {
-  let filePath = path.join(__dirname, '/../migrations/sqls', file);
+  const filePath = path.join(__dirname, '/../migrations/sqls', file);
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
       if (err) return reject(err);
-      console.log('Received data: ' + data);
+      console.log(JSON.stringify({
+        message: 'Migration processed',
+        migration: file,
+        sql: data,
+      }));
       resolve(data);
     });
   })
